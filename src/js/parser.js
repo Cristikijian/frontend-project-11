07b6a3b372fd40782d12fa215b4f449971b21a1e
Cristikijian <cristikijian@gmail.com>
2023-03-05 10:37:15 +0300

    refactoring 101

--- src/js/parser.js
+++ src/js/parser.js
@@ -3,7 +3,7 @@ export default function parser(data) {
   return new Promise((resolve, reject) => {
     const doc = parse.parseFromString(data, 'text/xml').documentElement;
     if (doc.querySelector('parsererror')) {
-      reject(new Error('error.parserError'));
+      reject(new Error('errors.parserError'));
     } else { resolve(doc); }
   });
 }
